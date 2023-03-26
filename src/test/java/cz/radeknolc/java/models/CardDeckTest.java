d371e3d7fc23fb9bfd96f483845ac54307fa2aab
Radek Nolč <radek.nolc@icloud.com>
2023-03-26 03:16:52 +0200

    Basic JUnit tests
    Created basic JUnit tests

---
+++ src/test/java/cz/radeknolc/java/models/CardDeckTest.java
@@ -0,0 +1,74 @@
+package cz.radeknolc.java.models;
+
+import cz.radeknolc.java.Generator;
+import cz.radeknolc.java.enums.CardRank;
+import cz.radeknolc.java.enums.CardSuit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class CardDeckTest {
+
+    @Test
+    public void testTotalValue() {
+        CardDeck cardDeck = new CardDeck(); //Creating deck to test
+        //Creating cards to add into deck
+        CardSuit suit = CardSuit.SUIT_CLUBS;
+        Card card = new Card(CardRank.RANK_A, suit);
+        cardDeck.add(card);
+        Assertions.assertEquals(11, cardDeck.getTotalValue());
+
+
+        cardDeck = new CardDeck(); //Creating new deck to test
+        //Creating cards to add into deck
+        card = new Card(CardRank.RANK_A, suit);
+        cardDeck.add(card);
+        card = new Card(CardRank.RANK_A, suit);
+        cardDeck.add(card);
+        Assertions.assertEquals(2, cardDeck.getTotalValue());
+
+        cardDeck = new CardDeck(); //Creating new deck to test
+        //Creating cards to add into deck
+        card = new Card(CardRank.RANK_10, suit);
+        cardDeck.add(card);
+        card = new Card(CardRank.RANK_A, suit);
+        cardDeck.add(card);
+        Assertions.assertEquals(21, cardDeck.getTotalValue());
+
+        cardDeck = Generator.generateDeck(); //Creating new deck to test
+        Assertions.assertEquals((95*4)-(4*10), cardDeck.getTotalValue());
+    }
+
+    @Test
+    public void testGetCard() {
+        CardDeck cardDeck = new CardDeck();
+
+        Card card1 = new Card(CardRank.RANK_Q, CardSuit.SUIT_HEARTS);
+        Card card2 = new Card(CardRank.RANK_K, CardSuit.SUIT_DIAMONDS);
+        Card card3 = new Card(CardRank.RANK_3, CardSuit.SUIT_CLUBS);
+        Card card4 = new Card(CardRank.RANK_9, CardSuit.SUIT_DIAMONDS);
+        Card card5 = new Card(CardRank.RANK_A, CardSuit.SUIT_CLUBS);
+        Card card6 = new Card(CardRank.RANK_K, CardSuit.SUIT_HEARTS);
+        Card card7 = new Card(CardRank.RANK_8, CardSuit.SUIT_SPADES);
+        Card card8 = new Card(CardRank.RANK_Q, CardSuit.SUIT_SPADES);
+
+        cardDeck.add(card1);
+        cardDeck.add(card2);
+        cardDeck.add(card3);
+        cardDeck.add(card4);
+        cardDeck.add(card5);
+        cardDeck.add(card6);
+        cardDeck.add(card7);
+        cardDeck.add(card8);
+
+        Assertions.assertEquals(card1, cardDeck.getCard(0));
+        Assertions.assertEquals(card2, cardDeck.getCard(1));
+        Assertions.assertEquals(card3, cardDeck.getCard(2));
+        Assertions.assertEquals(card4, cardDeck.getCard(3));
+        Assertions.assertEquals(card5, cardDeck.getCard(4));
+        Assertions.assertEquals(card6, cardDeck.getCard(5));
+        Assertions.assertEquals(card7, cardDeck.getCard(6));
+        Assertions.assertEquals(card8, cardDeck.getCard(7));
+    }
+}
---
+++ src/test/java/cz/radeknolc/java/models/SessionTest.java
@@ -0,0 +1,71 @@
+package cz.radeknolc.java.models;
+
+import cz.radeknolc.java.Generator;
+import cz.radeknolc.java.enums.CardRank;
+import cz.radeknolc.java.enums.CardSuit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SessionTest {
+
+    @Test
+    public void testCompare() {
+        CardDeck cardDeck = new CardDeck();
+
+        Player player = new Player("Player", false, 10000);
+        Player dealer = new Player("Dealer", true, 0);
+
+        for (int i = 1; i <= 4; i++) {
+            CardSuit suit = null;
+            switch(i) {
+                case 1 -> suit = CardSuit.SUIT_SPADES;
+                case 2 -> suit = CardSuit.SUIT_HEARTS;
+                case 3 -> suit = CardSuit.SUIT_CLUBS;
+                case 4 -> suit = CardSuit.SUIT_DIAMONDS;
+            }
+
+            Card[] cards = new Card[] {
+                    new Card(CardRank.RANK_2, suit),
+                    new Card(CardRank.RANK_3, suit),
+                    new Card(CardRank.RANK_4, suit),
+                    new Card(CardRank.RANK_5, suit),
+                    new Card(CardRank.RANK_6, suit),
+                    new Card(CardRank.RANK_7, suit),
+                    new Card(CardRank.RANK_8, suit),
+                    new Card(CardRank.RANK_9, suit),
+                    new Card(CardRank.RANK_10, suit),
+                    new Card(CardRank.RANK_J, suit),
+                    new Card(CardRank.RANK_Q, suit),
+                    new Card(CardRank.RANK_K, suit),
+                    new Card(CardRank.RANK_A, suit)
+            };
+
+            for (Card card : cards) {
+                cardDeck.add(card);
+            }
+        }
+
+        player.takeCard(cardDeck); // Taking 2
+        player.takeCard(cardDeck); // Taking 3
+        player.takeCard(cardDeck); // Taking 4
+        player.takeCard(cardDeck); // Taking 5
+        player.takeCard(cardDeck); // Taking 6
+
+        dealer.takeCard(cardDeck); // Taking 7
+        dealer.takeCard(cardDeck); // Taking 8
+        dealer.takeCard(cardDeck); // Taking 9
+
+        Assertions.assertEquals(player, new Session(player, dealer, cardDeck).compare());
+
+        player.returnCards(cardDeck);
+        dealer.returnCards(cardDeck);
+
+        player.takeCard(cardDeck); // Taking 10
+        player.takeCard(cardDeck); // Taking J
+
+        dealer.takeCard(cardDeck); // Taking Q
+        dealer.takeCard(cardDeck); // Taking K
+
+        Assertions.assertEquals(null, new Session(player, dealer, cardDeck).compare());
+    }
+}
